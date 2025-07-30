e64fd6327bf7bfc03b6c3345f525ef24f6018ec7
HarshChaudhari02 <harshalchaudhari623@gmail.com>
2025-07-30 18:01:51 +0530

    updates 30/07/25

--- python_practice/fileio.py
+++ python_practice/fileio.py
@@ -1,14 +1,57 @@
-with open("data.txt", "w") as file:
-    file.write("Hey! Newly Created")
+# with open("data.txt", "w") as file:
+#     file.write("Hey! Newly Created")
 
 
-with open("data.txt", "r") as file:
-    for line in file:
-        print(line)
+# with open("data.txt", "r") as file:
+#     for line in file:
+#         print(line)
 
 
-with open("data.txt", "a") as file:
-    file.write("Now Added")
+# with open("data.txt", "a") as file:
+#     file.write("Now Added")
 
-with open("data.txt", "r") as file:
-    print(file.read())
+# with open("data.txt", "r") as file:
+#     print(file.read())
+
+
+#CSV
+
+# import csv
+
+# filename = "data.csv"
+# data = [["Name", "Age"], ["Alice", 30], ["Bob", 25]]
+
+# with open(filename, "w", newline="") as file:
+#     writer = csv.writer(file)
+#     writer.writerows(data)
+
+
+# with open(filename, "r") as file:
+#     reader = csv.reader(file)
+#     for row in reader:
+#         print(row)
+
+
+#using dictioneries
+# import csv
+
+# data = [{"Name": "Alice", "Age": 30}, {"Name": "Bob", "Age": 25}]
+
+# with open("data.csv", "w", newline="") as file:
+#     fieldnames = ["Name", "Age"]
+#     writer = csv.DictWriter(file, fieldnames=fieldnames)
+#     writer.writeheader()
+#     writer.writerows(data)
+
+## Json
+
+import json
+
+data = {"Name": "Alice", "Age": 34, "Skills": ["Python", "SQL"]}
+
+with open("data.json", "w") as file:
+    json.dump(data, file, indent=4)
+
+with open("data.json", "r") as file:
+    out = json.load(file)
+    print(type(out))
---
+++ python_practice/pnd.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+## Reading
+
+# df = pd.read_csv("sales_data_sample.csv", encoding="latin1")
+# df = pd.read_json("data.json")
+# df = pd.read_excel("data.xlsx")
+
+# print(df.columns)
+# print(df.info())
+# print(df.describe())
+# print(df.head())
+# print(df.tail())
+# print(df.shape)
+
+
+
+## Writing
+
+# data = {
+#     "Name": ["alice", "bob", "tom"],
+#     "Age": [34, 24, 14],
+#     "Marks": [89, 99, 54]
+# }
+
+# df = pd.DataFrame(data)
+
+# df.to_excel("data.xlsx", index=False)
