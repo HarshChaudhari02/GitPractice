14db7b0792fab9114dd4a89081027a7d96611cc8
HarshChaudhari02 <harshalchaudhari623@gmail.com>
2025-07-30 11:20:41 +0530

    updates 29/7/25

--- learn_os.py
+++ learn_os.py
@@ -1,3 +1,9 @@
 import os
 
-print(os.getcwd())
+# print(os.getcwd())
+# files = os.listdir()
+# for file in files:
+#     print(file)
+
+if(not os.path.exists("python_practice")):
+    os.mkdir("python_practice")
