2ddc1fa7ae5f391696fa0d78ca61409a8b76074a
HarshChaudhari02 <harshalchaudhari623@gmail.com>
2025-08-01 22:54:37 +0530

    updates 01-08-2025

---
+++ python_practice/npy.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# lis = [98, 89, 88, 90]
+# print(lis)
+
+# arr = np.array([98, 89, 88, 90])
+# print(arr)
+
+## 2D array
+
+# arr = np.array([[1,2,3], [4,5,6]])
+# print(arr)
